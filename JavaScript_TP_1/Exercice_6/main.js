3365835ad04dae1a342fc58515b83ba362d32668
Mamba_Spirit <72410919+Mamba-Spirit@users.noreply.github.com>
2024-04-03 16:45:24 +0200

    Add files via upload

--- JavaScript_TP_1/Exercice_6/main.js
+++ JavaScript_TP_1/Exercice_6/main.js
@@ -1,1 +1,18 @@
-let myArray = [20,1,13,8,10,6,15,25,2,10,14,18,9];
+// Making a histogramme function :)
+function histogramme(tab){
+    let tabe_size = tab.length;
+    let motif = "";
+    for(let j = 0; j < tabe_size; j++){
+        for(let i = 0; i < tab[j]; i++){
+            motif +="@";
+        }
+        console.log(motif);
+        motif = ""; 
+    }
+}
+
+let my_array = [20,1,13,8,10,6,15,25,2,10,14,18,9];
+let tab = [18,13,18,20,18];
+
+histogramme(my_array);
+
